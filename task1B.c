8800689311e5c000f71fd7563e715da8030031a1
keen eyes <56186677+pelangi1@users.noreply.github.com>
2021-03-07 14:51:30 +0700

    hmmm

---
+++ task1B.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int find(int x, int arr){
+	int i;
+	
+	while( i < 100){
+		if (arr[i] == x);
+	}
+	return -1;
+}
+
+int main(){
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	if(find(9 arr) == -1) puts("Angka ada di array!");
+	puts("Angka tidak ada di array!");
+	
+	return 0;
+}
