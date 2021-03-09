8cec0ea8188575e89901f7e1b88388a61e5afa98
keen eyes <56186677+pelangi1@users.noreply.github.com>
2021-03-09 20:29:49 +0700

    Update modul1tugas2.c

--- modul1tugas2.c
+++ modul1tugas2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>   //isalnum, ascii
-#include <stdbool.h> //boolean
+#include <ctype.h> 
+#include <stdbool.h>
 
 int main(int argc, char** argv)
 {
