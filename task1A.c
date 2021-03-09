222ba17ff53e662289fecaa54fb5a3b5752a40cb
keen eyes <56186677+pelangi1@users.noreply.github.com>
2021-03-09 20:27:38 +0700

    perbaikan

--- task1A.c
+++ task1A.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int i=0, ganjil, genap, total;
-	
-    while (i < 10);
-    {	
-    	for(i=0; i<10; i++)
-        if (arr[i]%2 != 0) ganjil = arr[i];
-        if (arr[i]%2 == 0) genap = arr[i];
-        total += arr[i];
-        
-    }
+int main(){
+	int arr[]= {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i=0, ganjil=0, genap=0, total=0;
 
-    printf("Jumlah Bilangan Ganjil = %d\n", ganjil);
-    printf("Jumlah Bilangan Genap = %d\n", genap);
-    printf("Total = %d\n", total);
-    
-    return 0;
+	
+	while(i < 9){
+		if(arr[i]%2 == 0) {
+			genap++;
+		}
+		if(arr[i]%2 != 0){
+			ganjil++;
+		}
+		total =+ arr[i];
+		i++;
+	} 
+	
+	printf("jumlah bilangan ganjil = %d\n", ganjil);
+	printf("jumlah bilangan genap = %d\n", genap);
+	printf("total = %d\n", total);
+	return 0;
 }
