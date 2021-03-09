192ebd91adc6321d8e2e50fc069b937883356999
sanadayukimuragg <80328527+sanadayukimuragg@users.noreply.github.com>
2021-03-09 12:15:58 +0000

    Update task1A.c

--- task1A.c
+++ task1A.c
@@ -5,11 +5,11 @@ int main()
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int i=0, ganjil, genap, total;
 	
-    while (i > 10);
+    while (i < 10);
     {	
     	for(i=0; i<10; i++)
-        if (arr[i]%3 == 0) ganjil = arr[i];
-        if (arr[i]/2 == 0) genap = arr[i];
+        if (arr[i]%2 != 0) ganjil = arr[i];
+        if (arr[i]%2 == 0) genap = arr[i];
         total += arr[i];
         
     }
